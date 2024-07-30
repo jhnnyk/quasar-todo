7f82ef59bdf92cb6e0108bc2ce1e4ad5d2b8a87f
John Knotts <jhnnyk@gmail.com>
2024-07-30 16:58:55 -0600

    routes, layouts and a login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center" padding>
+    <div class="full-width" style="max-width: 300px">
+      <h5 class="q-my-md text-center">Login</h5>
+      <q-card>
+        <q-form>
+          <q-card-section>
+            <q-input
+              label="email"
+              placeholder="email@example.com"
+              class="q-mb-md"
+              type="email"
+              filled
+            />
+
+            <q-input label="password" class="q-mb-sm" type="password" filled />
+
+            <q-checkbox label="remember me" :model-value="true" />
+          </q-card-section>
+          <q-btn label="login" class="full-width" color="primary" />
+        </q-form>
+      </q-card>
+    </div>
+  </q-page>
+</template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,17 +2,22 @@ const routes = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/BlankLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+      { path: 'login', component: () => import('pages/LoginPage.vue') },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
+    component: () => import('pages/ErrorNotFound.vue'),
+  },
 ]
 
 export default routes
