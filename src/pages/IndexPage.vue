d28a29c58f8463be3e8b84d1848a727b87ebdc5b
John Knotts <jhnnyk@gmail.com>
2024-07-30 16:00:23 -0600

    set up main layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,36 @@
 <template>
-  <q-page padding>
-    <q-btn label="click me"></q-btn>
+  <q-page class="row">
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input
+          v-model="search"
+          placeholder="Search"
+          class="full-width"
+          borderless
+          dense
+        >
+          <template #append>
+            <q-icon name="mdi-magnify" />
+          </template>
+        </q-input>
+      </q-toolbar>
+      <q-list bordered>
+        <q-item>
+          <q-item-section side>
+            <q-checkbox :model-value="true" />
+          </q-item-section>
+          <q-item-section> Go shopping </q-item-section>
+          <q-item-section side>
+            <q-btn icon="mdi-delete" size="sm" flat round />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-card>
   </q-page>
 </template>
 
 <script setup>
-defineOptions({
-  name: 'IndexPage',
-})
+import { ref } from 'vue'
+
+const search = ref('')
 </script>
