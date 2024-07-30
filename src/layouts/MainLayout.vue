b05697cc57c89b024b1ab481b81e79fd248fc526
John Knotts <jhnnyk@gmail.com>
2024-07-30 17:13:57 -0600

    colors

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -32,7 +32,7 @@
           <q-item-section side>
             <q-icon name="mdi-shopping" />
           </q-item-section>
-          <q-item-section>Shopping List</q-item-section>
+          <q-item-section class="text-grey-9">Shopping List</q-item-section>
           <q-item-section side>22</q-item-section>
         </q-item>
       </q-list>
