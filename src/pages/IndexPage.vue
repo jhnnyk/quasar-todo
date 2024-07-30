9fd39bb78a6e3b382af7bcd7d9e9c8f97ea555e2
John Knotts <jhnnyk@gmail.com>
2024-07-30 15:43:08 -0600

    switch to mdi icons

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page padding>
+    <q-btn label="click me"></q-btn>
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
-});
+  name: 'IndexPage',
+})
 </script>
