954cd3b0b26f7e7e075c83ec4c43a45b745150f6
John Knotts <jhnnyk@gmail.com>
2024-07-30 18:10:17 -0600

    quasar plugins

--- src/components/CreateTodoButton.vue
+++ src/components/CreateTodoButton.vue
@@ -1,3 +1,40 @@
 <template>
-  <q-btn />
+  <q-btn @click="handleButtonClick" />
 </template>
+
+<script setup>
+import { useQuasar } from 'quasar'
+
+const $q = useQuasar()
+
+const handleButtonClick = () => {
+  $q.dialog({
+    title: 'Create Todo',
+    prompt: {
+      model: '',
+      type: 'text', // optional
+    },
+    cancel: true,
+    persistent: true,
+  })
+    // .onOk((data) => {
+    //   console.log('>>>> OK, received', data)
+    // })
+    .onOk(createTodo)
+    .onCancel(() => {
+      console.log('>>>> Cancel')
+    })
+    .onDismiss(() => {
+      console.log('I am triggered on both OK and Cancel')
+    })
+}
+
+const createTodo = (data) => {
+  console.log('adding todo: ', data)
+  $q.notify({
+    message: 'Todo created!',
+    icon: 'mdi-check',
+    color: 'positive',
+  })
+}
+</script>
